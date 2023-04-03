af9cd4975accde41e3367ed1c28fb14a70a7e2db
Sweets Sweetman <sweetmantech@gmail.com>
2023-04-03 09:22:23 -0300

    I init tokens JSON.

--- components/Pages/Rewards/tokens.ts
+++ components/Pages/Rewards/tokens.ts
@@ -2,14 +2,17 @@ const TOKENS = [
   {
     name: "Silver Builder Reward",
     address: "0xD300D8CB6003F4F72D37B5c2452e673c02327f5F",
+    requirement: "10 Builder Rewards",
   },
   {
     name: "Gold Builder Reward",
     address: "0xD300D8CB6003F4F72D37B5c2452e673c02327f5F",
+    requirement: "5 Silver Rewards",
   },
   {
     name: "Diamond Builder Reward",
     address: "0xD300D8CB6003F4F72D37B5c2452e673c02327f5F",
+    requirement: "2 Gold Rewards",
   },
 ]
 
