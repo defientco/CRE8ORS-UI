5ec7de9a38bebe15e5509688db4cbe951ba7108b
Software Engineer <im.ziad.sakr@gmail.com>
2023-07-26 13:35:48 -0400

    Update friendAndFamily.ts

--- lib/friendAndFamily.ts
+++ lib/friendAndFamily.ts
@@ -1,7 +1,7 @@
 import { ethers, Signer } from "ethers"
-import friendAndFamilyAbi from "../abi-friend-family.json"
-import handleTxError from "../handleTxError"
-import getDefaultProvider from "../getDefaultProvider"
+import friendAndFamilyAbi from "./abi-friend-family.json"
+import handleTxError from "./handleTxError"
+import getDefaultProvider from "./getDefaultProvider"
 
 export const hasDiscount = async (address: string) => {
   const provider = getDefaultProvider(process.env.NEXT_PUBLIC_TESTNET ? 5 : 1)
