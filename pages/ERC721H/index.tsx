8196a2aaff034180bae67acb48070e97c99ebdfb
Sweets Sweetman <sweetmantech@gmail.com>
2023-07-25 23:09:56 -0300

    I add SEO info.

--- pages/ERC721H/index.tsx
+++ pages/ERC721H/index.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router"
 import { useEffect } from "react"
+import SeoHead from "../../components/SeoHead"
 
 const ERC721H = () => {
   const router = useRouter()
@@ -8,7 +9,13 @@ const ERC721H = () => {
     router.push("https://mirror.xyz/sweetman.eth/gKpHCW-6wviwbQn_zzG7vQDZ-TxoV9GwWFdXaT_QzC4")
   }, [router])
 
-  return <div>loading</div>
+  return (
+    <SeoHead
+      title="ERC721H: A Standard for Programmable Hooks in NFTs"
+      description="ERC721H is an implementation of IERC721 that introduces 'Hooks', bringing additional flexibility and control to creators and collections."
+      image="/SEO_LOGO_ICON.png"
+    />
+  )
 }
 
 export default ERC721H
