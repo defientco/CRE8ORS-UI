e9ce9001d2512fc816c1dd20dafe7104aefe2aa9
Software Engineer <im.ziad.sakr@gmail.com>
2023-07-29 10:55:47 -0400

    update pre mint page and navbar

--- components/MintPage/MintPage.tsx
+++ components/MintPage/MintPage.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react"
 import Layout from "../Layout"
 import Cre8orsWay from "./Cre8orsWay"
 import PFPs from "./PFPs"
@@ -7,10 +8,14 @@ import Collaborate from "./Collaborate"
 import Footer from "../Footer"
 import PreMintBoard from "./PreMint/PreMintBoard"
 
-const MintPage = () => (
+interface MintPageProps {
+  type: "premint" | "mint"
+}
+
+const MintPage: FC<MintPageProps> = ({ type }) => (
   <Layout type="base">
     <div className="relative h-screen overflow-y-auto overflow-x-hidden">
-      <PreMintBoard />
+      {type === "premint" && <PreMintBoard />}
       <Cre8orsWay />
       <PFPs />
       <Archetypes />
