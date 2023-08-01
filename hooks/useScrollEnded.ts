6fb4b730bd8dd03adb4a213d95ef023ac0a01f86
SweetmanTech <SweetmanTech@users.noreply.github.com>
2023-08-01 21:29:33 +0000

    Apply formatting changes

--- hooks/useScrollEnded.ts
+++ hooks/useScrollEnded.ts
@@ -1,30 +1,29 @@
 import { MotionValue } from "framer-motion"
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 
 interface Props {
-    ref: any
-    scrollY: MotionValue<number>
+  ref: any
+  scrollY: MotionValue<number>
 }
 
-const useScrollEnded = ({
-    ref,
-    scrollY
-}: Props) => {
-    const [isScrollEnded, setIsScrollEnded] = useState(false)
+const useScrollEnded = ({ ref, scrollY }: Props) => {
+  const [isScrollEnded, setIsScrollEnded] = useState(false)
 
-    useEffect(() => {
-        if(scrollY && ref.current) {
-            scrollY.clearListeners()
-            scrollY.on('change', () => {
-                setIsScrollEnded(scrollY.get() === (ref.current.scrollHeight - ref.current.offsetHeight) 
-                && scrollY.get() > 0)
-            })
-        }
-    }, [ref, scrollY])
-
-    return {
-        isScrollEnded
+  useEffect(() => {
+    if (scrollY && ref.current) {
+      scrollY.clearListeners()
+      scrollY.on("change", () => {
+        setIsScrollEnded(
+          scrollY.get() === ref.current.scrollHeight - ref.current.offsetHeight &&
+            scrollY.get() > 0,
+        )
+      })
     }
+  }, [ref, scrollY])
+
+  return {
+    isScrollEnded,
+  }
 }
 
 export default useScrollEnded
