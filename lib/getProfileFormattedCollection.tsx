16982dedaa63e61d4ad0e363b3e27a87db9eb270
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-04 20:18:56 -0300

    I remove dev comments.

--- lib/getProfileFormattedCollection.tsx
+++ lib/getProfileFormattedCollection.tsx
@@ -3,13 +3,11 @@ import getNFTs from "./alchemy/getNFTs"
 const getProfileFormattedCollection = async (address) => {
   const response = await getNFTs(address, null, process.env.NEXT_PUBLIC_TESTNET ? 5 : 1)
 
-  // Transformation of the raw data
   const formattedData = response.ownedNfts.map((nft) => ({
-    label: nft.contractMetadata.name, // You can change this according to your specific requirements
+    label: nft.contractMetadata.name,
     type: nft.contract.address === process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS ? "cre8or" : undefined,
     isLocked: true,
     image: nft.media[0].thumbnail,
-    // Add additional properties if needed
   }))
 
   return formattedData
