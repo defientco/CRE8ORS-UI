8bebb2eb374f0e207b740d7e4f8cc0a07e12a51f
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-04 21:08:01 -0300

    I display nfts owned in my smart wallet.

--- components/ProfilePage/PreWalletCollection.tsx
+++ components/ProfilePage/PreWalletCollection.tsx
@@ -6,6 +6,7 @@ import UnlockModal from "./UnlockModal"
 import TrainModal from "./TrainModal"
 import { useProfileProvider } from "../../providers/ProfileContext"
 import getProfileFormattedCollection from "../../lib/getProfileFormattedCollection"
+import SmartWalletContents from "./SmartWalletContents"
 
 const PreWalletCollection = () => {
   const isMobile = useMediaQuery("(max-width: 1024px)")
@@ -87,42 +88,7 @@ const PreWalletCollection = () => {
               />
             </button>
           </div>
-          <div className="border-r-[2px] pr-[20px] lg:pr-[50px] border-r-[white]">
-            <div
-              className="mt-[35px]
-                    relative
-                    flex items-center justify-center
-                    lg:px-4 lg:py-6 p-2
-                    rounded-[8px] lg:rounded-[15px]
-                    overflow-hidden
-                    lg:w-[287px] lg:h-[287px]
-                    samsungS8:w-[130px] samsungS8:h-[130px]
-                    w-[120px] h-[120px]
-                    after:content-[''] after:bg-[white] after:opacity-[0.3]
-                    after:w-full after:h-full
-                    after:absolute
-                    after:left-0 after:top-0 after:z-[1]"
-            >
-              <div
-                className="absolute w-full h-full left-0 top-0 z-[2]
-              bg-[url('/assets/Profile/dna_animation.gif')] bg-cover"
-              />
-              <div
-                className="grid grid-cols-3 w-full relative z-[2]
-                        gap-y-[5px] lg:gap-y-[15px]"
-              >
-                {[...Array(9)].map((_, i) => (
-                  // eslint-disable-next-line react/no-array-index-key
-                  <div className="flex justify-center" key={i}>
-                    <div
-                      className="w-[30px] h-[30px] samsungS8:w-[35px] samsungS8:h-[35px] lg:w-[69px] lg:h-[67px] rounded-[5px] lg:rounded-[8px] bg-[#ffffffb5]
-                                        drop-shadow-[0_4px_4px_rgba(0,0,0,0.45)]"
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
+          <SmartWalletContents />
         </div>
         <div>
           <div className="flex items-center justify-end gap-x-[5px] samsungS8:gap-x-[10px]">
---
+++ components/ProfilePage/SmartWalletContents.tsx
@@ -0,0 +1,72 @@
+import { Contract } from "ethers"
+import { useEffect, useState } from "react"
+import registryAbi from "../../lib/abi-ERC6551-registry.json"
+import getDefaultProvider from "../../lib/getDefaultProvider"
+import { useProfileProvider } from "../../providers/ProfileContext"
+import getSmartWallet from "../../lib/getSmartWallet"
+import getNFTs from "../../lib/alchemy/getNFTs"
+import getProfileFormattedCollection from "../../lib/getProfileFormattedCollection"
+
+const SmartWalletContents = () => {
+  const { cre8orNumber } = useProfileProvider()
+  const [smartWalletAddress, setSmartWalletAddress] = useState()
+  const [ownedNfts, setOwnedNfts] = useState([])
+
+  useEffect(() => {
+    const init = async () => {
+      const smartWalletResponse = await getSmartWallet(cre8orNumber)
+      setSmartWalletAddress(smartWalletResponse)
+
+      const nftResponse = await getProfileFormattedCollection(smartWalletResponse)
+      setOwnedNfts(nftResponse)
+    }
+
+    init()
+  }, [cre8orNumber])
+
+  console.log("SWEETS tokenBoundAccount", smartWalletAddress)
+  console.log("SWEETS ownedNfts", ownedNfts)
+
+  return (
+    <div className="border-r-[2px] pr-[20px] lg:pr-[50px] border-r-[white]">
+      <div
+        className="mt-[35px]
+                    relative
+                    flex items-center justify-center
+                    lg:px-4 lg:py-6 p-2
+                    rounded-[8px] lg:rounded-[15px]
+                    overflow-hidden
+                    lg:w-[287px] lg:h-[287px]
+                    samsungS8:w-[130px] samsungS8:h-[130px]
+                    w-[120px] h-[120px]
+                    after:content-[''] after:bg-[white] after:opacity-[0.3]
+                    after:w-full after:h-full
+                    after:absolute
+                    after:left-0 after:top-0 after:z-[1]"
+      >
+        <div
+          className="absolute w-full h-full left-0 top-0 z-[2]
+              bg-[url('/assets/Profile/dna_animation.gif')] bg-cover"
+        />
+        <div
+          className="grid grid-cols-3 w-full relative z-[2]
+              gap-y-[5px] lg:gap-y-[15px]"
+        >
+          {ownedNfts?.map((nft, i) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div className="flex justify-center" key={i}>
+              <img
+                src={nft.image}
+                alt={nft.label} // Add an alt text if you have one
+                className="w-[30px] h-[30px] samsungS8:w-[35px] samsungS8:h-[35px] lg:w-[69px] lg:h-[67px] rounded-[5px] lg:rounded-[8px] bg-[#ffffffb5]
+                      drop-shadow-[0_4px_4px_rgba(0,0,0,0.45)]"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SmartWalletContents
