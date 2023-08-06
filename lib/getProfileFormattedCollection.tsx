3587906d23e16d750d37b574625bb717bf75d767
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-06 05:47:28 -0400

    collection filter functionality at collection content

--- lib/getProfileFormattedCollection.tsx
+++ lib/getProfileFormattedCollection.tsx
@@ -1,9 +1,20 @@
 import getNFTs from "./alchemy/getNFTs"
 
 const getProfileFormattedCollection = async (address) => {
-  const response = await getNFTs(address, null, process.env.NEXT_PUBLIC_TESTNET ? 5 : 1)
+  const cre8orCollections: any = await Promise.all(
+    [process.env.NEXT_PUBLIC_CLAIM_PASSPORT_ADDRESS, process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS].map(
+      async (contractAddress) => {
+        const response = await getNFTs(
+          address,
+          contractAddress,
+          process.env.NEXT_PUBLIC_TESTNET ? 5 : 1,
+        )
+        return response.ownedNfts
+      },
+    ),
+  )
 
-  const formattedData = response.ownedNfts.map((nft) => ({
+  const formattedData = cre8orCollections.flat().map((nft) => ({
     label: nft.contractMetadata.name,
     type: nft.contract.address === process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS ? "cre8or" : undefined,
     isLocked: true,
