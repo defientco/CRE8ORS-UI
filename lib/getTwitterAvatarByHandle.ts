5696a89415f822209fb77bbe8d8a17eee229a5ef
towardsnewtech <towardsnewtech@users.noreply.github.com>
2023-08-06 19:31:50 +0000

    Apply formatting changes

--- lib/getTwitterAvatarByHandle.ts
+++ lib/getTwitterAvatarByHandle.ts
@@ -1,12 +1,15 @@
-import axios from 'axios'
+import axios from "axios"
 
 export const getAvatarByTwitterHandle = async (twitterHandle: string) => {
   try {
-    const response = await axios.get(`https://api.twitter.com/1.1/users/show.json?screen_name=${twitterHandle}`, {
-      headers: {
-        Authorization: `Bearer ${process.env.TWITTER_BEARER}`
-      }
-    })    
+    const response = await axios.get(
+      `https://api.twitter.com/1.1/users/show.json?screen_name=${twitterHandle}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TWITTER_BEARER}`,
+        },
+      },
+    )
 
     return response?.data?.profile_image_url_https
   } catch (err) {
