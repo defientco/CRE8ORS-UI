0473acde5c8f79adec75295008eec9c7557f082d
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-09 09:44:38 -0300

    I update /mint SEO for Tweet info.

--- pages/mint/index.tsx
+++ pages/mint/index.tsx
@@ -1,8 +1,14 @@
 import MintPage from "../../components/MintPage"
+import SeoHead from "../../components/SeoHead"
 import { MintProvider } from "../../providers/MintProvider"
 
 const Mint = () => (
   <MintProvider>
+    <SeoHead
+      title="Cre8ors"
+      description="Minting Now."
+      image="https://nftstorage.link/ipfs/bafybeigzsxieetgynpr3l4lsmtukjs25e3tijrnalojivblf2qumlrlp5a"
+    />
     <MintPage type="mint" />
   </MintProvider>
 )
