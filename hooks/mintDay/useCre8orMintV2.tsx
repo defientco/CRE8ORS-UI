31e0d0dbcdd8984bba72a70adcfceaa035834f23
Vitaly Samonvv <im.ziad.sakr@gmail.com>
2023-08-18 17:06:29 -0400

    I integrated check chain network and wallet connect part.

--- hooks/mintDay/useCre8orMintV2.tsx
+++ hooks/mintDay/useCre8orMintV2.tsx
@@ -3,12 +3,21 @@ import cre8orAbi from "../../lib/abi-cre8ors.json"
 import { useEthersSigner } from "../useEthersSigner"
 import handleTxError from "../../lib/handleTxError"
 import { ethers } from "ethers"
+import useCheckNetwork from "../useCheckNetwork"
 
 const useCre8orMintV2 = () => {
   const signer = useEthersSigner({ chainId: process.env.NEXT_PUBLIC_TESTNET ? 5 : 1 })
+  const { checkNetwork } = useCheckNetwork()
 
   const mint = async (quantity) => {
     try {
+      if (!signer) {
+        throw new Error("Please, connect your wallet")
+      }
+      if (!checkNetwork()) {
+        throw new Error("switch your network")
+      }
+
       const contract = new Contract(process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS, cre8orAbi, signer)
 
       const value = ethers.utils.parseUnits(Number(0.05 * quantity).toString(), 18)
