a45bd55a88b3d51354d19cd41cdca07159b03ad3
Vitaly Samonvv <im.ziad.sakr@gmail.com>
2023-08-18 05:11:31 -0400

    I updated seo logo in layout component ✨

--- components/Layout/ContainedLayout.tsx
+++ components/Layout/ContainedLayout.tsx
@@ -1,10 +1,18 @@
 import React from "react"
+import { useRouter } from "next/router"
 import { ILayout } from "./types"
 import SeoHead from "../SeoHead"
 import Header from "../Header"
 import Footer from "../Footer/Footer"
 
-function ContainedLayout({ children }: ILayout) {
+const ContainedLayout = ({ children }: ILayout) => {
+  const router = useRouter()
+
+  const seoText = router.pathname.includes("mint")
+    ? "Minting Now."
+    : "Welcome to the next generation of creativity."
+  const seoLogo = router.pathname.includes("mint") ? "/v2-logo.png" : "/SEO_LOGO_ICON.png"
+
   return (
     <div className="min-h-[100vh] w-screen text-black bg-transparent dark:bg-[black] relative overflow-hidden">
       <div
@@ -12,11 +20,7 @@ function ContainedLayout({ children }: ILayout) {
           w-full h-[calc(100%+500px)]
           bg-[url('/assets/Layout/whitepaper.svg')] bg-cover bg-[center_center]"
       />
-      <SeoHead
-        title="Cre8ors"
-        description="Welcome to the next generation of creativity."
-        image="/SEO_LOGO_ICON.png"
-      />
+      <SeoHead title="Cre8ors" description={seoText} image={seoLogo} />
       <Header />
       <div className="flex justify-center relative z-[2]">
         <div className="md:w-[1280px] relative">{children}</div>
--- pages/mint/index.tsx
+++ pages/mint/index.tsx
@@ -1,14 +1,8 @@
 import MintPage from "../../components/MintPage"
-import SeoHead from "../../components/SeoHead"
 import { MintProvider } from "../../providers/MintProvider"
 
 const Mint = () => (
   <MintProvider>
-    <SeoHead
-      title="Cre8ors"
-      description="Minting Now."
-      image="/assets/SeoHead/v2-logo.jpg"
-    />
     <MintPage />
   </MintProvider>
 )
