70516b9dc6bcd83daa54aee4f01256b615bd7698
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-22 16:52:03 -0400

    I added error handling part while getting nfts

--- lib/alchemy/getNFTs.ts
+++ lib/alchemy/getNFTs.ts
@@ -11,19 +11,23 @@ const getNFTs = async (address: string, contractAddress: string, chainId: number
   const optionalContractParam = contractAddress ? `&contractAddresses%5B%5D=${contractAddress}` : ""
 
   while (1) {
-    const { data } = await axios.get(
-      `${getAlchemyBaseUrl(
-        chainId,
-      )}nft/v2/${alchemyKey}/getNFTs?owner=${address}${optionalContractParam}${
-        pageKey ? `&pageKey=${pageKey}` : ""
-      }`,
-    )
+    try {
+      const { data } = await axios.get(
+        `${getAlchemyBaseUrl(
+          chainId,
+        )}nft/v2/${alchemyKey}/getNFTs?owner=${address}${optionalContractParam}${
+          pageKey ? `&pageKey=${pageKey}` : ""
+        }`,
+      )
 
-    if (data.ownedNfts.length) nfts = nfts.concat(data.ownedNfts)
-    if (data.totalCount) totalCount = data.totalCount
+      if (data.ownedNfts.length) nfts = nfts.concat(data.ownedNfts)
+      if (data.totalCount) totalCount = data.totalCount
 
-    if (!data.pageKey) break
-    pageKey = data.pageKey
+      if (!data.pageKey) break
+      pageKey = data.pageKey
+    } catch (err) {
+      break
+    }
   }
 
   return {
