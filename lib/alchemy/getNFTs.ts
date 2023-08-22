d4ec4b973388db5336868caf61e5a66ebd53097b
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-22 16:32:40 -0400

    I updated getNFTs function for maximum page size

--- lib/alchemy/getNFTs.ts
+++ lib/alchemy/getNFTs.ts
@@ -1,15 +1,35 @@
+/* eslint-disable */
 import axios from "axios"
 import getAlchemyBaseUrl from "./getAlchemyBaseUrl"
 
 const getNFTs = async (address: string, contractAddress: string, chainId: number = 5) => {
+  let nfts = []
+  let pageKey = null
+  let totalCount = 0
+
   const alchemyKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
   const optionalContractParam = contractAddress ? `&contractAddresses%5B%5D=${contractAddress}` : ""
-  const { data } = await axios.get(
-    `${getAlchemyBaseUrl(
-      chainId,
-    )}nft/v2/${alchemyKey}/getNFTs?owner=${address}${optionalContractParam}`,
-  )
-  return data
+
+  while (1) {
+    const { data } = await axios.get(
+      `${getAlchemyBaseUrl(
+        chainId,
+      )}nft/v2/${alchemyKey}/getNFTs?owner=${address}${optionalContractParam}${
+        pageKey ? `&pageKey=${pageKey}` : ""
+      }`,
+    )
+
+    if (data.ownedNfts.length) nfts = nfts.concat(data.ownedNfts)
+    if (!totalCount) totalCount = data.totalCount
+
+    if (!data.pageKey) break
+    pageKey = data.pageKey
+  }
+
+  return {
+    ownedNfts: nfts,
+    totalCount,
+  }
 }
 
 export default getNFTs
