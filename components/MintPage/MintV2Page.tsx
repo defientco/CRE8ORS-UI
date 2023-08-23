52786e2af507a8c1f51f67029e4d7b0f17dcf3d4
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-23 14:59:10 -0300

    I fix line breaks

--- components/MintPage/MintV2Page.tsx
+++ components/MintPage/MintV2Page.tsx
@@ -92,7 +92,7 @@ const MintV2Page = () => {
             className="fade_in_text text-[15px] md:text-[20px] font-quicksand font-medium
                     text-center leading-[99.3%]"
           >
-            No lockup. No allowlist.\nFirst come, first serve.\nPrice: 0.05 ETH
+            {`No lockup. No allowlist.\nFirst come, first serve.\nPrice: 0.05 ETH`}
           </pre>
           <div
             className="flex justify-center items-center
