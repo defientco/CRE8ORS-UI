724107d825d8eb701496b3b2a53a3db5d2edf86a
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-26 20:03:43 -0300

    I fix build errors.

--- pages/api/v2/get/metadata/index.ts
+++ pages/api/v2/get/metadata/index.ts
@@ -1,13 +1,10 @@
 import { createHandler, Get, Query } from "next-api-decorators"
-import metadata from "../../../../../public/metadata/metadata.json"
 import getMetadata from "../../../../../lib/getMetadata"
 
 class GetMetadata {
   @Get()
   async metadata(@Query("tokenId") tokenId: string) {
-    const response = getMetadata(tokenId)
-    console.log("SWEETS GetMetadata API response", response)
-    return response
+    return getMetadata(tokenId)
   }
 }
 
