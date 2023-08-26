e6ccb5c742e2ca82bb66b6e3739618e1e3ac413e
Sweets Sweetman <sweetmantech@gmail.com>
2023-08-26 20:02:38 -0300

    I init formatMetadata lib.

--- lib/getMetadata.tsx
+++ lib/getMetadata.tsx
@@ -1,27 +1,19 @@
 import getDeterministicRandom from "./getDeterministicRandom"
 import feminine from "../public/metadata/raw/feminine.json"
 import masculineAndSpecial from "../public/metadata/raw/masculine_and_special.json"
+import formatMetadata from "./formatMetadata"
 
-const format = (raw, tokenId) => ({
-  ...raw,
-  name: `Cre8ors #${tokenId}`,
-  description: "A cult for creators.",
-})
+const MAX_TOKEN_ID = 8888
+const HALF_MAX = MAX_TOKEN_ID / 2
 
 const getMetadata = (tokenId) => {
-  // 1. get metadataID from tokenId
+  if (tokenId <= 0 || tokenId > MAX_TOKEN_ID) return {}
   const metadataId = getDeterministicRandom(tokenId)
-  console.log(`SWEETS TokenId ${tokenId} maps to ${metadataId}`)
-  // 2. get metadata
-  if (tokenId <= 0) return {}
-  if (tokenId <= 4444) {
-    return format(feminine.data[tokenId], tokenId)
+  if (metadataId <= HALF_MAX) {
+    return formatMetadata(feminine.data[tokenId], tokenId)
   }
-  if (tokenId <= 8888) {
-    const index = tokenId - 4444
-    return format(masculineAndSpecial.data[index], tokenId)
-  }
-  // 3. verify results
+  const index = metadataId - 4444
+  return formatMetadata(masculineAndSpecial.data[index], tokenId)
 }
 
 export default getMetadata
