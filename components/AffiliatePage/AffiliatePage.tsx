81302ec00543ab7d34aa7f916175674108f5d36e
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-30 20:46:06 -0400

    begin affiliatePage

---
+++ components/AffiliatePage/AffiliatePage.tsx
@@ -0,0 +1,15 @@
+import Layout from "../Layout"
+
+const AffiliatePage = () => {
+    return (
+        <Layout type="contained">
+            <div
+            className="relative w-[100%] min-h-[100vh]
+                    pt-[150px] md:pt-[250px]
+                    flex flex-col items-center gap-y-[20px] md:gap-y-[20px]"
+            ></div>
+        </Layout>
+    )
+}
+
+export default AffiliatePage
---
+++ pages/affiliate/index.tsx
@@ -0,0 +1,5 @@
+import AffiliatePage from "../../components/AffiliatePage"
+
+const Affiliate = () => <AffiliatePage />
+
+export default Affiliate
