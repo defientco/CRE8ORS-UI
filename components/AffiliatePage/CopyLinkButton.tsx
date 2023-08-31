89433a90b774e28346243178f098092ffee95541
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-31 16:46:09 -0400

    I added cre8or auto saving function

--- components/AffiliatePage/CopyLinkButton.tsx
+++ components/AffiliatePage/CopyLinkButton.tsx
@@ -1,25 +1,67 @@
 import { CopyToClipboard } from "react-copy-to-clipboard"
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { toast } from "react-toastify"
+import { useAccount } from "wagmi"
 import { useUserProvider } from "../../providers/UserProvider"
 import { Button } from "../../shared/Button"
 import Checkbox from "../../shared/Checkbox"
+import balanceOfAddress from "../../lib/balanceOfAddress"
+import useCre8orNumber from "../../hooks/mintDay/useCre8orNumber"
+import { updateUserCre8orNumber } from "../../lib/userInfo"
 
 const CopyLinkButton = ({ origin }) => {
-  const { cre8orNumber } = useUserProvider()
+  const { address } = useAccount()
+
+  const { cre8orNumber, getUserData } = useUserProvider()
+  const { getCre8orNumber } = useCre8orNumber({ address })
   const [isCopiedLink, setIsCopiedLink] = useState(false)
+  const [hasCre8or, setHasCre8or] = useState(false)
+
+  const getBalanceOf = useCallback(async () => {
+    if (address) {
+      const response = await balanceOfAddress(address)
+      if (parseInt(response.toString(), 10) > 0) {
+        setHasCre8or(true)
+        return
+      }
+
+      setHasCre8or(false)
+    }
+  }, [address])
+
+  const setCre8orNumber = useCallback(async () => {
+    if (hasCre8or && !cre8orNumber) {
+      const newCre8orNumber = await getCre8orNumber()
+      await updateUserCre8orNumber({
+        walletAddress: address,
+        cre8orNumber: newCre8orNumber,
+      })
+      getUserData()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [cre8orNumber, hasCre8or])
+
+  useEffect(() => {
+    getBalanceOf()
+  }, [getBalanceOf])
+
+  useEffect(() => {
+    setCre8orNumber()
+  }, [setCre8orNumber])
 
   return (
     <div className="flex gap-x-[15px] w-[280px]">
-      <CopyToClipboard text={`${origin}/mint?referral=${cre8orNumber}`}>
+      <CopyToClipboard text={cre8orNumber ? `${origin}/mint?referral=${cre8orNumber}` : ""}>
         <Button
           id="copy_link"
-          className="!p-0
-                      w-[240px] h-[46px]
-                      cursor-copy"
+          className={`!p-0
+          w-[240px] h-[46px]
+           ${cre8orNumber ? "cursor-copy" : "!bg-[gray] cursor-not-allowed"}`}
           onClick={() => {
-            toast.success("Copied to clipboard")
-            setIsCopiedLink(true)
+            if (cre8orNumber) {
+              toast.success("Copied to clipboard")
+              setIsCopiedLink(true)
+            }
           }}
         >
           copy affiliate link
