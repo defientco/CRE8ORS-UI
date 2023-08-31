b3d38c19501916aacb531530b18bdae0f86d3a96
Software Engineer <im.ziad.sakr@gmail.com>
2023-08-31 11:47:36 -0400

    I added connect wallet button

--- components/AffiliatePage/AffiliatePage.tsx
+++ components/AffiliatePage/AffiliatePage.tsx
@@ -1,20 +1,44 @@
+import { useAccount } from "wagmi"
 import Layout from "../Layout"
 import BuyCre8orButton from "./BuyCre8orButton"
 import CheckSmartWalletButton from "./CheckSmartWalletButton"
 import CopyLink from "./CopyLink"
+import WalletConnectButton from "../WalletConnectButton"
 
-const AffiliatePage = ({ origin }) => (
-  <Layout type="contained">
-    <div
-      className="relative w-[100%] min-h-[100vh]
-                    flex flex-col items-center justify-center
-                    gap-y-[20px] md:gap-y-[20px]"
-    >
-      <BuyCre8orButton />
-      <CheckSmartWalletButton />
-      <CopyLink origin={origin} />
-    </div>
-  </Layout>
-)
+const AffiliatePage = ({ origin }) => {
+  const { isConnected } = useAccount()
+
+  return (
+    <Layout type="contained">
+      <div
+        className="relative w-[100%] min-h-[100vh]
+                      flex flex-col items-center justify-center
+                      gap-y-[20px] md:gap-y-[20px]"
+      >
+        {isConnected ? (
+          <>
+            <BuyCre8orButton />
+            <CheckSmartWalletButton />
+            <CopyLink origin={origin} />
+          </>
+        ) : (
+          <WalletConnectButton>
+            <div
+              id="connect_wallet"
+              className="!p-0
+          w-[240px] h-[46px]
+          hover:scale-[1.1] scale-[1] transition duration-[300ms] font-bold font-quicksand 
+          uppercase text-white dark:text-[black] rounded bg-[black] dark:bg-[white] 
+          shadow-[0px_4px_4px_rgb(0,0,0,0.25)] dark:shadow-[0px_4px_4px_rgb(255,255,255,0.25)]
+          flex items-center justify-center gap-[10px]"
+            >
+              Connect Wallet
+            </div>
+          </WalletConnectButton>
+        )}
+      </div>
+    </Layout>
+  )
+}
 
 export default AffiliatePage
