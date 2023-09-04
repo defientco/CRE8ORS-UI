3290be4be2b471b1649bc8f0d0172709d0907ea0
Sweets Sweetman <sweetmantech@gmail.com>
2023-09-04 19:23:23 -0300

    I init lib getAffiliateMintEvents to get all AffiliateMint events.

---
+++ lib/getAffiliateMintEvents.tsx
@@ -0,0 +1,26 @@
+import { BigNumber, utils } from "ethers"
+import { getAddress } from "ethers/lib/utils"
+import { ethGetLogs } from "./alchemy/eth_getLogs"
+
+export const getAffiliateMintEvents = async () => {
+  const eventSignature = utils.id("AffiliateSale(address,uint256,uint256,uint256)")
+  const topics = [eventSignature]
+  console.log("SWEETS TOPICS", topics)
+  const chainId = process.env.NEXT_PUBLIC_TESTNET ? 5 : 1
+  const affiliateMinterAddress = process.env.NEXT_PUBLIC_AFFILIATE_MINTER
+  const rawLogs = await ethGetLogs(chainId, affiliateMinterAddress, topics)
+  console.log("SWEETS RAW LOGS", rawLogs)
+  const parsedLogs = rawLogs.map((log) => {
+    const userAddress = getAddress(`0x${log.topics[1].slice(26)}`)
+    const cre8orsNumber = BigNumber.from(log.topics[2]).toString()
+    const referralFeePaid = BigNumber.from(log.topics[3]).toString()
+
+    return {
+      userAddress,
+      cre8orsNumber,
+      referralFeePaid,
+    }
+  })
+
+  return parsedLogs.reverse()
+}
