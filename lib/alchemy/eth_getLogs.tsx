ab24c0b2936dd5b62be87b0ed3f1779cba043a55
Sweets Sweetman <sweetmantech@gmail.com>
2023-09-04 19:23:02 -0300

    I add alchemy util to get eth_getLogs function.

---
+++ lib/alchemy/eth_getLogs.tsx
@@ -0,0 +1,32 @@
+import axios from "axios"
+import getAlchemyBaseUrl from "./getAlchemyBaseUrl"
+
+// eslint-disable-next-line camelcase
+export const ethGetLogs = async (chainId, contractAddress, topics) => {
+  const endpoint = `${getAlchemyBaseUrl(chainId)}v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`
+
+  const payload = {
+    id: 0,
+    jsonrpc: "2.0",
+    method: "eth_getLogs",
+    params: [
+      {
+        fromBlock: "0x1", // Change this if you want to start from a specific block
+        toBlock: "latest",
+        address: contractAddress,
+        topics,
+      },
+    ],
+  }
+
+  try {
+    const response = await axios.post(endpoint, payload)
+    const logs = response.data.result
+
+    return logs
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error("Error fetching logs:", error)
+    return []
+  }
+}
