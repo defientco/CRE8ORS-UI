f2121e80a47e06c0fabd7491a9242500f2b98226
Software Engineer <im.ziad.sakr@gmail.com>
2023-09-10 10:05:20 -0400

    I updated dna cards showing on cre8ors collection

--- lib/getProfileFormattedCollection.tsx
+++ lib/getProfileFormattedCollection.tsx
@@ -10,7 +10,7 @@ const getProfileFormattedCollection = async (address, type) => {
   const collection = []
 
   if (type === SPECIALNFTS) {
-    const [cre8ors, passport] = await Promise.all([
+    const [cre8ors, passport, dnas] = await Promise.all([
       getNFTs(
         address,
         process.env.NEXT_PUBLIC_CRE8ORS_ADDRESS,
@@ -21,8 +21,13 @@ const getProfileFormattedCollection = async (address, type) => {
         process.env.NEXT_PUBLIC_CLAIM_PASSPORT_ADDRESS,
         process.env.NEXT_PUBLIC_TESTNET ? 5 : 1,
       ),
+      getNFTs(
+        address,
+        process.env.NEXT_PUBLIC_DNA_ADDRESS,
+        process.env.NEXT_PUBLIC_TESTNET ? 5 : 1,
+      ),
     ])
-    collection.push(...cre8ors.ownedNfts, ...passport.ownedNfts)
+    collection.push(...cre8ors.ownedNfts, ...passport.ownedNfts, ...dnas.ownedNfts)
   }
   if (type === ALLNFTS) {
     const response = await getNFTs(address, null, process.env.NEXT_PUBLIC_TESTNET ? 5 : 1)
